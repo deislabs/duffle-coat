3f5c89325d78a65079fece71fbd76d7740966787
itowlson <ivan.towlson@microsoft.com>
2018-10-28 14:19:53 -0700

    Interim commit of embedding binaries into generated code

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -2,6 +2,8 @@ import * as request from 'request-promise-native';
 import * as extract from 'extract-zip';
 import * as tmp from 'tmp';
 import { promisify } from 'util';
+import mkdirp = require('mkdirp');
+import * as tar from 'tar';
 
 import { fs } from './fs';
 import { Errorable, failed } from './errorable';
@@ -30,5 +32,45 @@ export async function downloadZip(source: string, destinationFolder: string): Pr
         return { succeeded: true, result: null };
     } catch (e) {
         return { succeeded: false, error: [`${e}`] };
+    }
+}
+
+export async function downloadTar(sourceUrl: string, destinationFolder: string): Promise<Errorable<null>> {
+    try {
+        const tempFileObj = tmp.fileSync({ prefix: "duffle-coat-" });
+        const downloadResult = await download(sourceUrl, tempFileObj.name);
+
+        if (failed(downloadResult)) {
+            return { succeeded: false, error: [`Failed to download ${sourceUrl}: error was ${downloadResult.error[0]}`] };
+        }
+
+        const tarfile = tempFileObj.name;
+
+        // untar it
+        const untarResult = await untar(tarfile, destinationFolder);
+        if (failed(untarResult)) {
+            return { succeeded: false, error: [`Failed to unpack ${sourceUrl}: error was ${untarResult.error[0]}`] };
+        }
+
+        tempFileObj.removeCallback();
+
+        return { succeeded: true, result: null };
+    } catch (e) {
+        return { succeeded: false, error: [`${e}`] };
+    }
+}
+
+async function untar(sourceFile: string, destinationFolder: string): Promise<Errorable<null>> {
+    try {
+        if (!(await fs.exists(destinationFolder))) {
+            mkdirp.sync(destinationFolder);
+        }
+        await tar.x({
+            cwd: destinationFolder,
+            file: sourceFile
+        });
+        return { succeeded: true, result: null };
+    } catch (e) {
+        return { succeeded: false, error: [`tar extract failed: ${e}`] };
     }
 }
